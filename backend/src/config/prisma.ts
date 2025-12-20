1f3c28c8bf6078ad5e78fd107503a96d510ab076
Suraj <skr.suraj26@gmail.com>
2025-12-20 16:46:20 +0530

    update

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,9 +1,6 @@
-import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
-  log: ["query", "error", "warn"],
-});
+export const prisma = new PrismaClient();
 
 process.on("SIGINT", async () => {
   await prisma.$disconnect();
