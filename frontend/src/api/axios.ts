0df1fa428fa4fe089a870d5423edae3db4458e27
Suraj <skr.suraj26@gmail.com>
2025-12-20 20:51:26 +0530

    update

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:
-    import.meta.env.MODE === "production"
-      ? "https://collaborative-task-manager-dx7z.onrender.com/api"
-      : "http://localhost:4000/api",
+  baseURL: "https://collaborative-task-manager-dx7z.onrender.com",
   withCredentials: true,
 });
